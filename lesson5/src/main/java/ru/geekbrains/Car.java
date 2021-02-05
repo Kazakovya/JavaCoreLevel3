a3fd0f1fc2cc4bace350380dbaa424db99c93b1a
Kazakov <kazakov.ya@gmail.com>
2021-02-05 14:31:30 +0300

    дз5

---
+++ lesson5/src/main/java/ru/geekbrains/Car.java
@@ -0,0 +1,55 @@
+package ru.geekbrains;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT = 0;
+    private static boolean winner;
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier cb;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier cb) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.cb = cb;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cb.await();
+            cb.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWinner(this);
+            cb.await();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized void checkWinner(Car c) {
+        if (!winner) {
+            System.out.println(c.name + " - WIN");
+            winner = true;
+        }
+    }
+}
+
